3e6b2f9bfda338f53ca4e53e7d03e61ee15f8b59
Brandon L. Gauthier <Brandon.L.Gauthier@gmail.com>
2015-05-11 22:58:47 -0400

    lessons 14-21 (end)

--- app/models/todo.js
+++ app/models/todo.js
@@ -5,24 +5,4 @@ var Todo = DS.Model.extend({
   isCompleted: DS.attr('boolean')
 });
 
-Todo.reopenClass({
-    FIXTURES: [
-        {
-           id: 1,
-           title: 'Learn Ember.js',
-           isCompleted: true
-         },
-         {
-           id: 2,
-           title: 'asdf',
-           isCompleted: false
-         },
-         {
-           id: 3,
-           title: 'Profit!',
-           isCompleted: false
-         }
-    ]
-});
-
 export default Todo;
